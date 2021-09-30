9535e023c4951370271ee9deaf3f01ce2dfd4ded
Victor Klawitter <victor.klawitter@gmail.com>
2021-09-30 12:17:44 -0400

    fix: adjusted the model

--- Server/models/ItemsModel.js
+++ Server/models/ItemsModel.js
@@ -3,15 +3,17 @@ module.exports = (sequelize, DataTypes) => {
   const items = sequelize.define('items', {
     title: DataTypes.STRING,
     category: DataTypes.STRING,
-    brand: DataTypes.STRING,
     image: DataTypes.STRING,
-    productId: DataTypes.STRING,
-    productUrl: DataTypes.STRING,
     primaryKey: {
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true
-    }
+    },
+    brand: DataTypes.STRING,
+    productId: DataTypes.STRING,
+    productUrl: DataTypes.STRING,
+   
+    
   });
   return items; 
 };
