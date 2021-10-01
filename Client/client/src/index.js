a882ef1cb14bd1323e5290023bf8ba3c97b17f82
Sean Araujo <seanaraujo93@gmail.com>
2021-10-01 18:16:31 -0400

    TS for LoginPage

--- Client/client/src/index.js
+++ Client/client/src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './redux/reducers';
 import './index.css';
-import App from './App';
+import App from './App.tsx';
 import reportWebVitals from './reportWebVitals';
 import {
   BrowserRouter as Router,
